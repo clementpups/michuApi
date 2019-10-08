f61c76bd59cf161fb5b9adf0dcd20e62ffbd1c16
Biggy <jululien.gautier@laposte.net>
2019-10-08 15:35:17 +0200

    controller

---
+++ MichuAPI/Controllers/ProduitController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MichuAPI.Controllers
+{
+    public class ProduitController : ApiController
+    {
+        // GET: api/Produit
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Produit/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Produit
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Produit/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Produit/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MichuAPI/Controllers/RecetteController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MichuAPI.Controllers
+{
+    public class RecetteController : ApiController
+    {
+        // GET: api/Recette
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Recette/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Recette
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Recette/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Recette/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
