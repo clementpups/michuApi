0bc45d44b42e88dc86c01ecf90fbcb0ed246a523
Hardouin-Brochard <rakoon@outlook.fr>
2019-10-08 16:07:55 +0200

    Ajout DAL Produit
    ajout de la DAL Produit

---
+++ MichuAPI/DAL/Devise.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MichuAPI.DAL
+{
+    /// <summary>
+    /// énumération représentant la devise du prix du produit
+    /// </summary>
+    public enum Devise
+    {
+        Euros = 0,
+        Dollar = 1,
+        Undefined = 99
+    }
+}
---
+++ MichuAPI/DAL/Produit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace MichuAPI.DAL
+{
+    /// <summary>
+    /// classe représentant un produit
+    /// </summary>
+    public class Produit
+    {
+        public Produit()
+        {
+        }
+
+        public string Nom { get; set; }
+        public double Prix { get; set; }
+        public Devise Devise { get; set; }
+        public string PrixPoid { get; set; }
+        public string QuantiteeContionnement { get; set; }
+
+        /// <summary>
+        /// Fonction d'extraction du prix et de la devise du prix
+        /// </summary>
+        /// <param name="sPrix"></param>
+        public void ExtrairePrixEtDevise(string sPrix)
+        {
+            //récupération du symbole de la devise 
+            char radicalDevise = sPrix[sPrix.Length - 1];
+            //récupération du prix en double
+            double prix = Double.Parse(sPrix.Substring(0, sPrix.Length - 1));
+
+            this.Prix = prix;
+            this.SetDevise(radicalDevise);
+
+        }
+
+        /// <summary>
+        /// assignement d'une devide en fonction d'un radical de devise 
+        /// </summary>
+        /// <param name="radicalDevise">radical de la devise en Char (symbole monétaire)</param>
+        private void SetDevise(char radicalDevise)
+        {
+            switch (radicalDevise)
+            {
+                case '€':
+                    this.Devise = Devise.Euros;
+                    break;
+                case '$':
+                    this.Devise = Devise.Dollar;
+                    break;
+                default:
+                    this.Devise = Devise.Undefined;
+                    break;
+            }
+        }
+
+    }
+}
